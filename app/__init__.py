e59c3b67b2ba73da4c903fbe13bfcbd35af30991
Bpbrinson <BrandonBrinson1@gmail.com>
2025-08-27 21:47:53 -0400

    Initial Commit

---
+++ app/__init__.py
@@ -0,0 +1,11 @@
+# import Flask to create flask instance
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('config.Config')
+
+    from .routes import main
+    app.register_blueprint(main)
+    
+    return app
